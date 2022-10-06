249b0380c86f319b380d5dc6e8ff93db6c376e7f
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2022-10-06 12:50:30 -0300

    feat: add carousel

--- my_portifolio/src/pages/Home.jsx
+++ my_portifolio/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../components/Header';
+import ControlledCarousel from '../components/Projects'
 
 function Home() {
   return (
     <main>
       <Header />
-      
+      <ControlledCarousel />
     </main>
   )
 }
